13a1797784567308400db9f22acb01e636f51e06
krasnyy <55034491+krasnyy@users.noreply.github.com>
2020-03-24 18:23:17 +0300

    asp.netcore

---
+++ MyServerUpload/Controllers/FileController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyServerUpload.Models;
+
+namespace MyServerUpload.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+
+        [HttpPost("upload")]
+        public async Task<ActionResult> Upload(IFormFile file)
+        {
+            try
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/images");
+                var stream = new FileStream(path, FileMode.Create);
+                file.CopyToAsync(stream);
+                return Ok(new { length = file.Length, name = file.Name });
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            
+        }
+
+
+        [HttpPost("uploads")]
+        public async Task<IActionResult> Uploads(List<IFormFile>files)
+        {
+            try
+            {
+                var result = new List<FileUploadResult>();
+                foreach (var file in files)
+                {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images");
+                    var stream = new FileStream(path, FileMode.Create);
+                    file.CopyToAsync(stream);
+                    result.Add(new FileUploadResult() { Name = file.FileName, Length = file.Length });
+
+                }
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+
+    }
+}
---
+++ MyServerUpload/Models/FileuPLOADResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyServerUpload.Models
+{
+    public class FileUploadResult
+    {
+        public string Name { get; set; }
+        public long Length { get; set; }
+
+    }
+}
